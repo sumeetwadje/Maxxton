602a9fa34fc859e1a7008c6ea653ac19e18b1251
sumeetwadje <wadje.sumeet7@gmail.com>
2021-04-25 20:06:34 +0530

    changes

--- src/app/component/employee-details/employee-details.component.ts
+++ src/app/component/employee-details/employee-details.component.ts
@@ -5,7 +5,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
-  styleUrls: ['./employee-details.component.css']
+  styleUrls: ['./employee-details.component.css'] 
 })
 export class EmployeeDetailsComponent implements OnInit {
 
